d8f0b37feabb8d617a0ea504c3cc4d8e2713438c
Zhang Yifeng <zhangyf@shanghaitech.edu.cn>
2019-05-08 23:17:17 +0800

    draft

--- utils.py
+++ utils.py
@@ -5,8 +5,7 @@ import networkx as nx
 
 
 def argmax(d):
-    """ a) create a list of the dict's keys and values; 
-        b) return the key with the max value"""  
+    # d is dict
     v = list(d.values())
     k = list(d.keys())
     return k[v.index(max(v))]
